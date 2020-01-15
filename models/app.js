50f87a380ddfb587c9a42aa4ccc1ba675afb21d7
Stephen Peters <7speter@gmail.com>
2020-01-15 00:29:10 -0500

    Moves app.js to models creates paymentIntent.js there too

--- models/app.js
+++ models/app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 
 const PORT = process.env.PORT || 3000
 
@@ -11,10 +12,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(morgan('short'));
 
+
 //app.set('view engine', 'handlebars' )
 
-app.use(require('./routes'));
-
+app.use(require('../routes'));
+dotenv.config();
 
 app.listen(PORT, ()=>{
     console.log(`Server is up and listening on port ${PORT}...`)
