df43decd4758c18178d226f674a612f74be4bd43
Stephen Peters <7speter@gmail.com>
2020-01-18 01:03:49 -0500

    Sends public key to checkout view

--- routes/paymentForm.js
+++ routes/paymentForm.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const key_vars = require('../models/key_vars.js');
 
 router.get('/', (req, res)=>{
-    res.render('pages/checkout.hbs', {title: "Checkout Element"})
+    res.render('pages/checkout.hbs', {title: "Checkout Element", client_secret: key_vars.returns_public_key})
     console.log(key_vars.returns_public_key);
 });
 
