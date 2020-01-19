f46c0c85ee21e11b96e538d829b4699e5dae9ec6
Stephen Peters <7speter@gmail.com>
2020-01-19 11:07:19 -0500

    Added then deleted a line in paymentform.js

--- routes/paymentForm.js
+++ routes/paymentForm.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const key_vars = require('../models/key_vars.js');
 
+
 router.get('/', (req, res)=>{
     res.render('pages/checkout.hbs', {title: "Checkout Element", client_secret: key_vars.returns_public_key})
     console.log(key_vars.returns_public_key);
